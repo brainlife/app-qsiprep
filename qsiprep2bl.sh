05c7926e91ae4f70fba1d699d038aeb25a0c3aab
gberto <gberto@fbk.eu>
2022-01-06 20:50:04 -0500

    update

--- qsiprep2bl.sh
+++ qsiprep2bl.sh
@@ -60,6 +60,6 @@ if [ $xflip == "true" ]; then
     echo "flip x-bvecs to be compatible with MRtrix"
     sed -i '$s/}/,\n"bvecs_out":".\/output_dwi\/dwi.bvecs"}/' config.json
     singularity exec -e docker://brainlife/mcr:neurodebian1604-r2017a ./compiled/main
-    rm -rf output_dwi/dwi.bvecs
+    rm output_dwi/dwi.bvecs
     cp dwi.bvecs output_dwi/dwi.bvecs
 fi
