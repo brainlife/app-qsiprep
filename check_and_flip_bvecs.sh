0973dc8bb817e248e3c4b25e9d2c3faf7b034533
gberto <gberto@fbk.eu>
2022-03-15 20:03:36 -0400

    bug fix

--- check_and_flip_bvecs.sh
+++ check_and_flip_bvecs.sh
@@ -9,29 +9,28 @@ SINGULARITYENV_PYTHONNOUSERSITE=true singularity exec -e docker://brainlife/dipy
 msg=$(jq -r '.brainlife[].msg' product.json)
 bvecs=$(jq -r '.bvecs[0]' config.json)
 
-#backup config.json
-mv config.json config_original.json
-
 if [[ "$msg" =~ "bvecs directions look good!" ]]; then
 	echo "No bvecs flip is needed!"
 	cp $bvecs ./dwi_new.bvecs
-elif [[ "$msg" =~ "bvecs-x seems to be flipped." ]]; then
-	echo "Flipping bvecs-x..."
-	echo "{\"bvecs_out\": \"$bvecs\", \"xflip\": \"true\"}" > config.json
+
+else
+	mv config.json config_original.json #backup config.json
+
+	if [[ "$msg" =~ "bvecs-x seems to be flipped." ]]; then
+		echo "Flipping bvecs-x..."
+		echo "{\"bvecs_out\": \"$bvecs\", \"xflip\": \"true\"}" > config.json
+	elif [[ "$msg" =~ "bvecs-y seems to be flipped." ]]; then
+		echo "Flipping bvecs-y..."
+		echo "{\"bvecs_out\": \"$bvecs\", \"yflip\": \"true\"}" > config.json
+	elif [[ "$msg" =~ "bvecs-z seems to be flipped." ]]; then
+		echo "Flipping bvecs-z..."
+		echo "{\"bvecs_out\": \"$bvecs\", \"zflip\": \"true\"}" > config.json
+	fi
+
     singularity exec -e docker://brainlife/mcr:neurodebian1604-r2017a ./compiled/main
     cp dwi.bvecs ./dwi_new.bvecs #flipped bvecs
-elif [[ "$msg" =~ "bvecs-y seems to be flipped." ]]; then
-	echo "Flipping bvecs-y..."
-	echo "{\"bvecs_out\": \"$bvecs\", \"yflip\": \"true\"}" > config.json
-    singularity exec -e docker://brainlife/mcr:neurodebian1604-r2017a ./compiled/main
-    cp dwi.bvecs ./dwi_new.bvecs #flipped bvecs
-elif [[ "$msg" =~ "bvecs-z seems to be flipped." ]]; then
-	echo "Flipping bvecs-z..."
-	echo "{\"bvecs_out\": \"$bvecs\", \"zflip\": \"true\"}" > config.json
-    singularity exec -e docker://brainlife/mcr:neurodebian1604-r2017a ./compiled/main
-    cp dwi.bvecs ./dwi_new.bvecs #flipped bvecs
+	rm config.json 
+	mv config_original.json config.json #restore original config.json
 fi
 
-#restore original config.json
-rm config.json 
-mv config_original.json config.json
+
