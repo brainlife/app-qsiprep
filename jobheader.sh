09ccf253ab0ce672fcdc04f1306997e7da158d93
Anibal Sólon <anibalsolon@gmail.com>
2023-10-10 13:14:12 -0500

    fix: walltime logic seems to be flipped

--- jobheader.sh
+++ jobheader.sh
@@ -2,9 +2,9 @@
 
 ##figure out amount of wall time we really need by parsing config.json with jq
 if [[ $(jq -r .dwi config.json) == *","* ]]; then
-    walltime=12:00:00
-else
     walltime=20:00:00
+else
+    walltime=12:00:00
 fi
 
 echo "#PBS -l nodes=1:ppn=8,vmem=50gb"
